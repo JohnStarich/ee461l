92ef3553c24d9449a5950199d4240552d4c6543b
jjalfaro9 <jalfaro@utexas.edu>
2016-04-01 00:00:50 -0500

    Created a few simple tests and they all passed. Search method does return a list of Movie objects.

--- src/test/java/com/johnstarich/moviematcher/models/MovieTest.java
+++ src/test/java/com/johnstarich/moviematcher/models/MovieTest.java
@@ -1,7 +1,23 @@
 package com.johnstarich.moviematcher.models;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by Josue on 3/31/2016.
  */
 public class MovieTest {
+    @Test
+    public void moviesSearchShouldReturnRelevantResults() {
+        java.util.List<Movie> movieList = Movie.search("Creed");
+        assertNotEquals(0, movieList.size());
+
+        movieList = Movie.search("The Dark Knight");
+        assertNotEquals(0, movieList.size());
+        assertEquals(true, movieList.get(0).title.contains("Dark"));
+
+        movieList = Movie.search("aklsjdflkj ajjasdkfj ajsfojoasjdfl");
+        assertEquals(0, movieList.size());
+
+    }
 }
