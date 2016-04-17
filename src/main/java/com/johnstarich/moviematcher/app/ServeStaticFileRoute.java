d110de9789d7c2c4c104f5957e0123abecca0c1a
John Starich <john.starich@thirdship.com>
2016-04-17 17:20:16 -0500

    Add development mode for static file handler to not cache files

--- src/main/java/com/johnstarich/moviematcher/app/ServeStaticFileRoute.java
+++ src/main/java/com/johnstarich/moviematcher/app/ServeStaticFileRoute.java
@@ -1,5 +1,6 @@
 package com.johnstarich.moviematcher.app;
 
+import com.johnstarich.moviematcher.store.ConfigManager;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -18,68 +19,69 @@ import java.util.concurrent.ConcurrentHashMap;
  * Render an HTML file as a String we can send in a response body.
  */
 public class ServeStaticFileRoute implements Route {
-    private static final String STATIC_PREFIX = "static";
-    private static final Map<String, StaticFile> loadedFiles = new ConcurrentHashMap<>(16);
+	private static final String STATIC_PREFIX = "static";
+	private static final Map<String, StaticFile> loadedFiles = new ConcurrentHashMap<>(16);
 
-    private final String file;
+	private final String file;
 
-    public ServeStaticFileRoute() {
-        this.file = null;
-    }
+	public ServeStaticFileRoute() {
+		this.file = null;
+	}
 
-    public ServeStaticFileRoute(String file) {
-        this.file = file;
-    }
+	public ServeStaticFileRoute(String file) {
+		this.file = file;
+	}
 
-    @Override
-    public Object handle(Request request, Response response) throws Exception {
-        StaticFile staticFile = getPage(this.file != null ? this.file : request.uri());
+	@Override
+	public Object handle(Request request, Response response) throws Exception {
+		StaticFile staticFile = getPage(this.file != null ? this.file : request.uri());
 
-        response.type(staticFile.contentType);
-        return staticFile.content;
-    }
+		response.type(staticFile.contentType);
+		return staticFile.content;
+	}
 
-    private static StaticFile getPage(String path) throws HttpException {
-        StringBuilder relativeFilePathBuilder = new StringBuilder(STATIC_PREFIX);
-        if(! path.startsWith("/")) relativeFilePathBuilder.append('/');
-        relativeFilePathBuilder.append(path);
-        String relativeFilePath = relativeFilePathBuilder.toString();
+	private static StaticFile getPage(String path) throws HttpException {
+		StringBuilder relativeFilePathBuilder = new StringBuilder(STATIC_PREFIX);
+		if(! path.startsWith("/")) relativeFilePathBuilder.append('/');
+		relativeFilePathBuilder.append(path);
+		String relativeFilePath = relativeFilePathBuilder.toString();
 
-        if(loadedFiles.containsKey(relativeFilePath)) {
-            return loadedFiles.get(relativeFilePath);
-        }
+		if(ConfigManager.getPropertyOrDefault("ENVIRONMENT", "development").equals("production") &&
+			loadedFiles.containsKey(relativeFilePath)) {
+			return loadedFiles.get(relativeFilePath);
+		}
 
-        try {
-            URL url = MovieMatcherApplication.class.getClassLoader().getResource(relativeFilePath);
-            if (url != null) {
-                Path filePath = Paths.get(url.toURI());
-                String fileContents = new String(Files.readAllBytes(filePath), Charset.defaultCharset());
-                String fileContentType = getContentType(path);
-                StaticFile staticFile = new StaticFile(fileContents, fileContentType);
-                loadedFiles.put(relativeFilePath, staticFile);
-                return staticFile;
-            }
-            else throw new HttpException(HttpStatus.NOT_FOUND);
-        }
-        catch(URISyntaxException | IOException e) {
-            throw new HttpException(HttpStatus.NOT_FOUND);
-        }
-    }
+		try {
+			URL url = MovieMatcherApplication.class.getClassLoader().getResource(relativeFilePath);
+			if (url != null) {
+				Path filePath = Paths.get(url.toURI());
+				String fileContents = new String(Files.readAllBytes(filePath), Charset.defaultCharset());
+				String fileContentType = getContentType(path);
+				StaticFile staticFile = new StaticFile(fileContents, fileContentType);
+				loadedFiles.put(relativeFilePath, staticFile);
+				return staticFile;
+			}
+			else throw new HttpException(HttpStatus.NOT_FOUND);
+		}
+		catch(URISyntaxException | IOException e) {
+			throw new HttpException(HttpStatus.NOT_FOUND);
+		}
+	}
 
-    private static String getContentType(String fileName) {
-        String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
-        switch(extension) {
-            case "css": return "text/css";
-            case "gif": return "image/gif";
-            case "html": return "text/html";
-            case "ico": return "image/x-icon";
-            case "jpg":case "jpeg": return "image/jpeg";
-            case "js": return "application/javascript";
-            case "png": return "image/png";
-            case "svg": return "image/svg+xml";
-            case "txt": return "text/plain";
-            case "xml": return "application/xml";
-            default: return "application/octet-stream";
-        }
-    }
+	private static String getContentType(String fileName) {
+		String extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+		switch(extension) {
+			case "css": return "text/css";
+			case "gif": return "image/gif";
+			case "html": return "text/html";
+			case "ico": return "image/x-icon";
+			case "jpg":case "jpeg": return "image/jpeg";
+			case "js": return "application/javascript";
+			case "png": return "image/png";
+			case "svg": return "image/svg+xml";
+			case "txt": return "text/plain";
+			case "xml": return "application/xml";
+			default: return "application/octet-stream";
+		}
+	}
 }
