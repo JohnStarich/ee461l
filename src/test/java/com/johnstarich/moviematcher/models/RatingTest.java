8584d78cec0afa3061a7b128d8e52807df074521
jjalfaro9 <jalfaro@utexas.edu>
2016-04-21 10:41:47 -0500

    Changed Rating.equals(Object o) to provide safe gaurding against nullptrs. We now throw an IllegalArgumentException when numeric_rating is out of bounds.

---
+++ src/test/java/com/johnstarich/moviematcher/models/RatingTest.java
@@ -0,0 +1,14 @@
+package com.johnstarich.moviematcher.models;
+
+import com.johnstarich.moviematcher.app.AbstractMongoDBTest;
+import junit.framework.TestCase;
+
+/**
+ * Created by Josue on 4/21/2016.
+ */
+public class RatingTest extends AbstractMongoDBTest {
+    public void testEquals() throws Exception {
+
+    }
+
+}
