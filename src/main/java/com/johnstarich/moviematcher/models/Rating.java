7b78b00e443018fe5788db179bdc1e19d14a72bc
jjalfaro9 <jalfaro@utexas.edu>
2016-04-25 10:58:21 -0500

    Rating Tests & Minor Rating Class Modifications
    * Finished RatingTest. Adjusted Rating class to have an integer based rating. Also implemented Rating.loadRatingsByUser
    
    * Added one test in testEquals().

--- src/main/java/com/johnstarich/moviematcher/models/Rating.java
+++ src/main/java/com/johnstarich/moviematcher/models/Rating.java
@@ -1,6 +1,9 @@
 package com.johnstarich.moviematcher.models;
 
+import com.johnstarich.moviematcher.store.MovieMatcherDatabase;
 import org.bson.types.ObjectId;
+
+import java.util.List;
 
 /**
  * Created by Josue on 4/17/2016.
@@ -9,17 +12,17 @@ public class Rating extends AbstractModel<Rating> {
     public final ObjectId user_id;
     public final ObjectId movie_id;
     public final String comment;
-    public final byte numeric_rating; //ratings are [0,10]
+    public final int numeric_rating; //ratings are [0,10]
 
     public Rating(ObjectId rating_id) {
         super(Rating.class, rating_id);
         this.user_id = null;
         this.movie_id = null;
-        this.comment = "";
-        this.numeric_rating = Byte.MIN_VALUE;
+        this.comment = null;
+        this.numeric_rating = Integer.MIN_VALUE;
     }
 
-    public Rating(ObjectId rating_id, ObjectId user_id, ObjectId movie_id, String comment, byte numeric_rating) {
+    public Rating(ObjectId rating_id, ObjectId user_id, ObjectId movie_id, String comment, int numeric_rating) {
         super(Rating.class, rating_id);
         this.user_id = user_id;
         this.movie_id = movie_id;
@@ -32,9 +35,12 @@ public class Rating extends AbstractModel<Rating> {
 
     @Override
     public boolean equals(Object o) {
-        if(o == null || !(o instanceof Rating)) return false;
-        if( ( (Rating) o).id == null) return false;
-        return ( (Rating) o).id.equals(id);
+        if (o == null || !(o instanceof Rating)) return false;
+        return ((Rating) o).id != null && ((Rating) o).id.equals(id);
     }
-}
 
+    public static List<Rating> loadRatingsByUser(ObjectId userID) {
+        return MovieMatcherDatabase.morphium.findByField(Rating.class, "user_id", userID);
+    }
+
+}
--- src/test/java/com/johnstarich/moviematcher/models/RatingTest.java
+++ src/test/java/com/johnstarich/moviematcher/models/RatingTest.java
@@ -2,13 +2,43 @@ package com.johnstarich.moviematcher.models;
 
 import com.johnstarich.moviematcher.app.AbstractMongoDBTest;
 import junit.framework.TestCase;
+import org.bson.types.ObjectId;
 
 /**
  * Created by Josue on 4/21/2016.
  */
 public class RatingTest extends AbstractMongoDBTest {
-    public void testEquals() throws Exception {
+    private User Maximus = new User(new ObjectId(), "MaximusDecimusMeridius@Gladiator.com", "Maximus" , "Meridius");
+    private User Commodus = new User(new ObjectId(), "Commodus@EmperorKilla.com", "Commodus", "Aurelius");
+    private Movie Gladiator = new Movie(new ObjectId(), "Gladiator", null, null, null, null, null, null, null);
+    private Rating rating1 = new Rating(new ObjectId(), Maximus.id, Gladiator.id, "Best movie ever. I love that last fight scene", 10 );
+    private Rating rating2 = new Rating(new ObjectId(), Commodus.id, Gladiator.id, "It was okay. It was only cool when I was in the movie", 5);
 
+    public void testEquals() throws Exception {
+        assertFalse(rating1.equals(rating2));
+        assertTrue(rating2.equals(rating2));
+    }
+
+    public void testLoadRatingsByUser() {
+        rating1 = rating1.save();
+        Movie theNiceGuys = new Movie(new ObjectId(), "The Nice Guys", null, null, null, null, null, null, null);
+        new Rating(new ObjectId(), Maximus.id, theNiceGuys.id, "Purely Awesome!", 10).save();
+
+        assertEquals(2, Rating.loadRatingsByUser(Maximus.id).size());
+        assertEquals(0, Rating.loadRatingsByUser(Commodus.id).size());
+    }
+
+    public void testExists() {
+        rating1 = rating1.save();
+        assertTrue(rating1.exists());
+        assertFalse(rating2.exists());
+    }
+
+    public void testDelete() {
+        rating2 = rating2.save();
+        assertTrue(rating2.exists());
+        rating2 = rating2.delete();
+        assertFalse(rating2.exists());
     }
 
 }
