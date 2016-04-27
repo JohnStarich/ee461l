a40ecad8ab6ca62b7c0c3590f28d2fc195b22eff
Jeremy Castillo <jeremy.castillo14@utexas.edu>
2016-04-26 21:14:30 -0500

    implemented register route. and have skeleton for login

--- src/main/java/com/johnstarich/moviematcher/app/MovieMatcherApplication.java
+++ src/main/java/com/johnstarich/moviematcher/app/MovieMatcherApplication.java
@@ -162,6 +162,7 @@ public class MovieMatcherApplication extends JsonApplication {
 		jget("/movies/search/", searchRoute);
 		jget("/movies/search", searchRoute);
 
+
 		Route movieRoute = (request, response) -> {
 			String movieId = request.params("id");
 			System.out.println("Looked up movie with ID: "+movieId);
