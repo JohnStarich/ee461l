551328feb74bf4a80269ecce50ceae8ac8eb2cbc
jjalfaro9 <jalfaro@utexas.edu>
2016-05-04 23:17:41 -0500

    progress with algorithm

--- src/main/java/com/johnstarich/moviematcher/models/Group.java
+++ src/main/java/com/johnstarich/moviematcher/models/Group.java
@@ -7,11 +7,11 @@ import de.caluga.morphium.annotations.Reference;
 import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
