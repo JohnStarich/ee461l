a250cec5b1b3c45767767f34286b50ff407840d9
jjalfaro9 <jalfaro@utexas.edu>
2016-05-04 23:17:41 -0500

    progress with algorithm

--- src/main/java/com/johnstarich/moviematcher/models/Group.java
+++ src/main/java/com/johnstarich/moviematcher/models/Group.java
@@ -1,14 +1,19 @@
 package com.johnstarich.moviematcher.models;
 
+import com.johnstarich.moviematcher.app.HttpException;
+import com.johnstarich.moviematcher.app.HttpStatus;
 import de.caluga.morphium.annotations.Index;
 import de.caluga.morphium.annotations.Reference;
 import org.bson.types.ObjectId;
 
 import javax.crypto.spec.OAEPParameterSpec;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by Josue on 4/16/2016.
@@ -59,14 +64,40 @@ public class Group extends AbstractModel<Group> {
         return ((Group) o).name != null && ((Group) o).name.equals(name) && ((Group) o).members.size() == members.size();
     }
 
-    public Optional<List<Movie>> suggestMovies() {
+    public Optional<List<Movie>> suggestMovies() throws HttpException {
        /* bring the ratings in O(Users) */
         if(members == null) return Optional.empty();
-        members.parallelStream().filter(user -> {Rating.loadRatingsByUser(user.id).});
-       /* filter ratings (all of them) */
+
+        //really don't want to create local variable ....
+        List<Rating> groupRatings = new ArrayList<>();
+        members.parallelStream().forEach(
+            user -> {
+                Optional<List<Rating>> r = Rating.loadRatingsByUser(user.id);
+                if(r.isPresent()) r.get().parallelStream().forEach(groupRatings::add);
+            }
+        );
+
+        if(groupRatings.isEmpty()) throw new HttpException(HttpStatus.BAD_REQUEST, "Please have each user in " +name+" rate more movies!");
+        /* filter ratings (all of them) */
        /* what do we want to filter ratings by (greater than 3 or 4) */
+        List<Rating> GroupRatings = groupRatings.parallelStream().filter(movieRating -> movieRating.numeric_rating >= 4).collect(Collectors.toList());
+
        /* let us go load the movies (We have to load the movies) (but if we can filter down to smaller subset, that's perfect)*/
+        List<Optional<Movie>> movies = new ArrayList<>();
+        GroupRatings.parallelStream().forEach(rating -> movies.add(new Movie(new ObjectId(rating.movie_id.toString())).load(Movie.class)));
+
        /* for every movie , split genre and throw it into the map */
+        List<String> genres = new ArrayList<>();
+        movies.parallelStream().forEach(
+            movie -> {
+                if(movie.isPresent()) {
+                    if(movie.get().genre != null)
+                        genres.addAll(Arrays.asList(movie.get().genre.split(", ")));
+                }
+            }
+        );
+
+
         return Optional.empty();
     }
 }
