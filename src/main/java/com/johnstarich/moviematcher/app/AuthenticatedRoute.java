085efd36860b1050031d1312d6d7a77e8ba254dd
John Starich <john.starich@thirdship.com>
2016-05-17 22:56:40 -0500

    Add authenticated routes which provide the user attribute (#61)

---
+++ src/main/java/com/johnstarich/moviematcher/app/AuthenticatedRoute.java
@@ -0,0 +1,14 @@
+package com.johnstarich.moviematcher.app;
+
+import com.johnstarich.moviematcher.models.User;
+import spark.Request;
+import spark.Response;
+
+/**
+ * Routes provided with the current authenticated user.
+ * Created by johnstarich on 5/16/16.
+ */
+@FunctionalInterface
+public interface AuthenticatedRoute {
+	Object handle(Request request, Response response, User user) throws Exception;
+}
