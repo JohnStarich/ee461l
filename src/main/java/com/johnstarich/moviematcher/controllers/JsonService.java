dce738a7cc45773e06ed3048b900e24689d5c774
John Starich <john.starich@thirdship.com>
2016-05-29 19:17:58 -0500

    Fix json attribute not created for /v1/users

--- src/main/java/com/johnstarich/moviematcher/controllers/JsonService.java
+++ src/main/java/com/johnstarich/moviematcher/controllers/JsonService.java
@@ -4,6 +4,8 @@ import com.google.gson.JsonSyntaxException;
 import com.johnstarich.moviematcher.models.User;
 import com.johnstarich.moviematcher.routes.AuthenticatedRoute;
 import com.johnstarich.moviematcher.routes.JsonTransformer;
+import org.json.simple.parser.ParseException;
+import spark.Filter;
 import spark.Request;
 import spark.Route;
 
@@ -27,26 +29,28 @@ public abstract class JsonService extends AbstractService {
 	@Override
 	public final void init() {
 		super.init();
-		before(PREFIX+"/*", (req, resp) -> {
+		Filter jsonAttribute = (req, resp) -> {
 			resp.type("application/json");
 			Map jsonMap = null;
 			if(req.contentType() != null && req.contentType().contains("application/json")) {
 				try {
 					jsonMap = json.parse(req.body(), Map.class);
 				}
-				catch (JsonSyntaxException e) {
+				catch (JsonSyntaxException | ParseException e) {
 					// ignore syntax errors: any unknown fields, if used, will be empty Optionals
 				}
 			}
 			req.attribute("json", Optional.ofNullable(jsonMap));
-		});
+		};
+		before(PREFIX+"/*", jsonAttribute);
+		before(PREFIX, jsonAttribute);
 		this.initService();
 	}
 
 	public abstract void initService();
 
 	public <T> Optional<T> bodyParam(Request request, String key) {
-		Optional<Map<String, T>> mapOptional =  request.attribute("json");
+		Optional<Map<String, T>> mapOptional = request.attribute("json");
 		if(! mapOptional.isPresent()) return Optional.empty();
 		return Optional.ofNullable(mapOptional.get().get(key));
 	}
